985e3238e09f957c748d1cb18c43ca78f2f4bcd3
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:17:40 +0530

    Assignment Operators

---
+++ JavaScript/JS Fundamental/Operators/assignmentOperator.js
@@ -0,0 +1,11 @@
+// Assignment Operator
+
+let a = 5;
+let b = 2;
+
+console.log("a += 4 = ", a += 4);    // a += 4 =  9
+console.log("a -= 4 = ", a -= 4);    // a -= 4 = 1
+console.log("a *= 4 = ", a *= 4);    // a *= 4 =  20
+console.log("a /= 4 = ", a /= 4);    // a /= 4 =  1.25
+console.log("a %= 4 = ", a %= 4);    // a %= 4 =  1
+console.log("a **= 4 = ", a **= 4);    // a **= 4 =  625
