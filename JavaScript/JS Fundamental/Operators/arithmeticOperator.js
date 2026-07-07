57d2b7ab29489d98dd5019d2676a756510d643eb
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:17:08 +0530

    Arithmetic Operators

---
+++ JavaScript/JS Fundamental/Operators/arithmeticOperator.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators
+
+let a = 5;
+let b = 10;
+
+// Addition
+console.log("a + b = ", a + b);     // a + b =  15
+
+// Subtraction
+console.log("a - b = ", a - b);     // a - b =  -5
+
+// Multiplication
+console.log("a * b = ", a * b);     // a * b =  50
+
+// Division
+console.log("a / b = ", a / b);     // a / b =  0.5
+
+// Modulus
+console.log("a % b = ", a % b);     // a % b =  5
+
+// Exponentiation
+console.log("a ** b = ", a ** b);     // a ** b =  9765625
+
+
+// Unary Operators
+
+// Increment
+console.log("a++ = ", a++);     // a++ =  5
+console.log("a = ", a);         // a = 6
+// Decrement
+
+console.log("b-- = ", b--);     // b-- =  10
+console.log("b = ", b);         // b = 9
