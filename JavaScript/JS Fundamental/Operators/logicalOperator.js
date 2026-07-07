8a9c7ad8101afb455a1f34f17b0698fbba623f9f
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:18:23 +0530

    Logical Operators

---
+++ JavaScript/JS Fundamental/Operators/logicalOperator.js
@@ -0,0 +1,17 @@
+// Logical Operator
+
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;  // true
+let cond2 = a === b;  // true
+
+// Logical AND
+console.log("cond1 && cond2 = ", cond1 && cond2);    // cond1 && cond2 =  false
+
+// Logical OR
+console.log("cond1 || cond2 = ", cond1 || cond2);    // cond1 || cond2 =  true
+
+// Logical NOT
+console.log("!cond1 = ", !cond1);    // !cond1 =  false
+console.log("!cond2 = ", !cond2);    // !cond2 =  true
