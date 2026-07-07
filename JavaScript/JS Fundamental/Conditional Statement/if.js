25dc37b1c6dcffb7872da47d2d937b70ca406f27
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:21:08 +0530

    If statement

---
+++ JavaScript/JS Fundamental/Conditional Statement/if.js
@@ -0,0 +1,27 @@
+let age = 25;
+
+if (age >= 18 ) {
+    console.log("You can vote");
+} 
+
+if (age <= 18 ) {
+    console.log("You cannot vote");
+} 
+
+// Output -- You can vote
+
+
+let mode = "dark";
+let color;
+
+if(mode === "dark") {
+    color = "black";
+}
+
+if(mode === "light") {
+    color = "white";
+} 
+
+console.log(color);
+
+// Output -- black
