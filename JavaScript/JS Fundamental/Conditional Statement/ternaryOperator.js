7098ee093a1bbb2f0c9135867ee3dbab4021c4ab
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:22:40 +0530

    Ternary Operators

---
+++ JavaScript/JS Fundamental/Conditional Statement/ternaryOperator.js
@@ -0,0 +1,9 @@
+// Ternary Operator --- a ? b : c
+// condition ? true output : false output
+
+let age = 20;
+let result = age >= 18 ? "adult" : "not adult";
+
+console.log(result); 
+
+// Output -- adult
