7b511e3e10ab48a30d2f7eed141bacdfa0077a0d
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:21:26 +0530

    If-else statement

---
+++ JavaScript/JS Fundamental/Conditional Statement/if-else.js
@@ -0,0 +1,38 @@
+let age = 25;
+
+if (age >= 18 ) {
+    console.log("You can vote");
+} 
+else {
+    console.log("You cannot vote");
+} 
+
+// Output -- You can vote
+
+
+let mode = "light";
+let color;
+
+if(mode === "dark") {
+    color = "black";
+}
+else {
+    color = "white";
+} 
+
+console.log(color);
+
+// Output -- white
+
+
+// Even or Odd
+let num = 10;
+
+if(num % 2 === 0) {
+    console.log(num, "is even");
+}
+else {
+    console.log(num, "is odd");
+}
+
+// Output -- 10 is even
