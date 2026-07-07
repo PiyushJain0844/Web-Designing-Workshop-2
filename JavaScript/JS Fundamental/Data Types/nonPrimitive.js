880c9a569679e46f9a210c480a44c89cdc27a765
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 17:29:53 +0530

    Non Primitive Datatype

---
+++ JavaScript/JS Fundamental/Data Types/nonPrimitive.js
@@ -0,0 +1,20 @@
+// Non-Primitive Data Types
+
+// Objects
+const student = {
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : 8.2,
+    isPass : true,
+};
+
+console.log(student);      // { fullName: 'Rahul Kumar', age: 20, cgpa: 8.2, isPass: true }
+console.log(typeof student);     // object
+
+//Both will give same value -> 20
+console.log(student["age"]); 
+console.log(student.age); 
+
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]);     // 21
