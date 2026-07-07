eb91525f265ab7a7df863342cb8c0ddb567f3f8a
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 17:31:06 +0530

    Primitive Datatype

---
+++ JavaScript/JS Fundamental/Data Types/primitive.js
@@ -0,0 +1,36 @@
+//Primitive Data Types
+
+// Number
+let age = 24;
+console.log(age);  // 24
+console.log(typeof age);  // number
+
+// String
+let name = "Raj";
+console.log(name);   //Raj
+console.log(typeof name);  // string
+
+// Boolean
+isFollow = true;
+console.log(isFollow);  // true 
+console.log(typeof isFollow);  // boolean
+
+// Undefined
+let x;
+console.log(x);  // undefined
+console.log(typeof x);  // undefined
+
+// Null
+let y = null;
+console.log(y);  // null
+console.log(typeof y);  // object
+
+// BigInt
+let z = BigInt("123");
+console.log(z);  // 123n
+console.log(typeof z);  // bigint
+
+// Symbol
+let a = Symbol("Hello");
+console.log(a);  // Symbol(Hello)
+console.log(typeof a);  // symbol 
