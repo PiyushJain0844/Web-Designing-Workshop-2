6d73b34eae2a101efa6cfa6bf78e5a1b821ee396
Piyush Jain <piyushjain230906@gmail.com>
2026-07-07 22:21:47 +0530

    else-if statement

---
+++ JavaScript/JS Fundamental/Conditional Statement/else-if.js
@@ -0,0 +1,19 @@
+let mode = "silver";
+let color;
+
+if(mode === "dark") {
+    color = "black";
+}
+else if(mode === "blue") {
+    color = blue;
+}
+else if(mode === "pink") {
+    color = pink;
+}
+else {
+    color = "white";
+} 
+
+console.log(color);
+
+// Output -- white
