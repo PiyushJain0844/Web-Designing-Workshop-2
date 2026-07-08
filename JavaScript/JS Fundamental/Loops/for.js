2703998106051d7bdf5981852b8f51c2502acaf3
Piyush Jain <piyushjain230906@gmail.com>
2026-07-08 23:46:36 +0530

    for loop

---
+++ JavaScript/JS Fundamental/Loops/for.js
@@ -0,0 +1,14 @@
+// Print 5 times
+for(let i = 1; i <= 5; i++) {
+    console.log("Hello");
+} 
+
+
+// Calculate sum of 1 to 5
+let sum = 0;
+for(let i = 1; i <= 5; i++) {
+    sum = sum + i;
+} 
+console.log("Sum =", sum);  
+
+// OUTPUT -- Sum = 15 
