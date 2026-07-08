fefe1d963a2ed4c3c792fb1658ab1310acb1083c
Piyush Jain <piyushjain230906@gmail.com>
2026-07-08 23:47:56 +0530

    for-of loop

---
+++ JavaScript/JS Fundamental/Loops/for-of.js
@@ -0,0 +1,14 @@
+let str = "Hello";
+let size = 0;
+for(let val of str) {
+    console.log(val);
+    size++;
+}
+console.log("String size = ", size);
+
+// H
+// e
+// l
+// l
+// o
+// String size =  5 
